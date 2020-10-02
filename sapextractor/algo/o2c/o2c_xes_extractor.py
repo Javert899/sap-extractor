7bcd5f85b11809a56faf828dc9305fc2ccb0b367
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-10-02 11:25:21 +0200

    graph connected components

--- sapextractor/algo/o2c/o2c_xes_extractor.py
+++ sapextractor/algo/o2c/o2c_xes_extractor.py
@@ -1,4 +1,5 @@
 from sapextractor.algo.o2c import o2c_common
+from sapextractor.utils.graph_building import build_graph
 
 
 def apply(con):
@@ -8,4 +9,7 @@ def apply(con):
     cols["event_activity"] = "concept:name"
     cols["event_timestamp"] = "time:timestamp"
     dataframe = dataframe.rename(columns=cols)
-    print(dataframe)
+    ancest_succ = build_graph.get_ancestors_successors(dataframe, "VBELV", "VBELN", "VBTYP_V", "VBTYP_N",
+                                                       ref_type="Order")
+    dataframe = dataframe.merge(ancest_succ, left_on="VBELN", right_on="node", suffixes=('', '_r'))
+    return dataframe
--- sapextractor/utils/graph_building/build_graph.py
+++ sapextractor/utils/graph_building/build_graph.py
@@ -1,5 +1,58 @@
-def apply(df, prev, curr):
+import networkx as nx
+import pandas as pd
+
+
+def apply(df, prev, curr, prev_type, curr_type):
+    prev_nodes = set(df.dropna(subset=[prev], how="any")[prev].unique())
+    succ_nodes = set(df.dropna(subset=[curr], how="any")[curr].unique())
+    all_nodes = prev_nodes.union(succ_nodes)
+    edges = set()
     df = df.dropna(subset=[prev, curr], how="any")
-    stream = df.to_dict('r')
+    stream = df[[prev, curr, prev_type, curr_type]].to_dict('r')
+    types = {}
     for el in stream:
-        pass
+        source = el[prev]
+        target = el[curr]
+        type_source = el[prev_type]
+        type_target = el[curr_type]
+        types[source] = type_source
+        types[target] = type_target
+        if source is not None and target is not None:
+            if str(source).lower() != "nan" and str(target).lower():
+                edges.add((source, target))
+    G = nx.DiGraph()
+    for node in all_nodes:
+        G.add_node(node)
+    for edge in edges:
+        G.add_edge(edge[0], edge[1])
+    return G, types
+
+
+def get_conn_comp(df, prev, curr, prev_type, curr_type):
+    G, types = apply(df, prev, curr, prev_type, curr_type)
+    conn_comp = nx.connected_components(nx.Graph(G))
+    ret = {}
+    for index, cc in enumerate(conn_comp):
+        for node in cc:
+            ret[node] = str(index)
+    return ret
+
+
+def get_ancestors_successors(df, prev, curr, prev_type, curr_type, ref_type=""):
+    G, types = apply(df, prev, curr, prev_type, curr_type)
+    list_corresp = []
+
+    nodes_ref_type = {x for x,y in types.items() if y == ref_type}
+
+    for index, node in enumerate(nodes_ref_type):
+        all_ancestors = set(nx.ancestors(G, node))
+        all_descendants = set(nx.ancestors(G, node))
+        all_nodes = all_ancestors.union(all_descendants).union({node})
+        for n2 in all_nodes:
+            list_corresp.append({"node": n2, "case:concept:name": index})
+
+    dataframe = pd.DataFrame(list_corresp).sort_values("node")
+
+    return dataframe
+
+
