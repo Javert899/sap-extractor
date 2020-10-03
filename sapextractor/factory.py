70418a8e7db19e70437d8a6148864e3be44b8baa
Alessandro Berti <javert@northwood.northwood.net>
2020-10-03 09:06:28 +0200

    rename

--- sapextractor/factory.py
+++ sapextractor/factory.py
@@ -2,6 +2,6 @@ from sapextractor.database_connection import factory as conn_factory
 from sapextractor.algo import factory as algo_factory
 
 
-def extraction_with_arguments(db_type, db_con_arg, process, ext_type, ext_arg):
+def apply(db_type, db_con_arg, process, ext_type, ext_arg):
     con = conn_factory.apply(db_type, db_con_arg)
     return algo_factory.apply(con, process, ext_type, ext_arg)
