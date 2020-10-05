19f7571cd3e234f7817c034398dc1e5d2c3edf9c
Alessandro Berti <javert@northwood.northwood.net>
2020-10-05 09:20:55 +0200

    added example
    added example

---
+++ examples/ap_ar_doc_flow_transactions.py
@@ -0,0 +1,11 @@
+import sapextractor
+
+
+def execute_script():
+    con = sapextractor.connect_sqlite("../sap.sqlite")
+    dataframe = sapextractor.get_ap_ar_doc_flow_transactions_dataframe(con, ref_type="Goods receipt")
+    dataframe.to_csv("ap_ar_document_flow_transactions.csv", index=False)
+
+
+if __name__ == "__main__":
+    execute_script()
