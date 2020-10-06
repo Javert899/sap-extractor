9d580e85fe902ce6294e7f681c6f67bbc45ae768
Alessandro Berti <javert@northwood.northwood.net>
2020-10-06 08:22:07 +0200

    keep the first for EBAN and EKKO

--- sapextractor/algo/p2p/tab_processing/eban_processing.py
+++ sapextractor/algo/p2p/tab_processing/eban_processing.py
@@ -5,6 +5,8 @@ def apply(con):
     eban = con.prepare_and_execute_query("EBAN", ["BANFN", "ERNAM", "ERDAT"])
     eban.columns = ["event_"+x for x in eban.columns]
     eban = eban.rename(columns={"event_ERNAM": "event_USERNAME", "event_ERDAT": "event_timestamp"})
+    eban = eban.sort_values("event_timestamp")
+    eban = eban.groupby("event_BANFN").first().reset_index()
     eban["event_timestamp"] = pd.to_datetime(eban["event_timestamp"])
     eban["event_activity"] = "Create Purchase Requisition"
     eban["event_FROMTABLE"] = "EBAN"
--- sapextractor/algo/p2p/tab_processing/ekko_processing.py
+++ sapextractor/algo/p2p/tab_processing/ekko_processing.py
@@ -5,6 +5,8 @@ def apply(con):
     ekko = con.prepare_and_execute_query("EKKO", ["EBELN", "ERNAM", "AEDAT", "LIFNR", "ZTERM"])
     ekko.columns = ["event_"+x for x in ekko.columns]
     ekko = ekko.rename(columns={"event_ERNAM": "event_USERNAME", "event_AEDAT": "event_timestamp"})
+    ekko = ekko.sort_values("event_timestamp")
+    ekko = ekko.groupby("event_EBELN").first().reset_index()
     ekko["event_timestamp"] = pd.to_datetime(ekko["event_timestamp"])
     ekko["event_activity"] = "Create Purchase Order"
     ekko["event_FROMTABLE"] = "EKKO"
