db0d43cef963bc9257a2a6b9061de70468d42a7f
Alessandro Berti <javert@northwood.northwood.net>
2020-10-06 09:45:14 +0200

    fix

--- sapextractor/algo/factory.py
+++ sapextractor/algo/factory.py
@@ -1,5 +1,6 @@
 from sapextractor.algo.ap_ar import factory as ap_ar_factory
 from sapextractor.algo.o2c import factory as o2c_factory
+from sapextractor.algo.p2p import factory as p2p_factory
 
 
 def apply(con, process, ext_type, ext_arg):
@@ -7,3 +8,5 @@ def apply(con, process, ext_type, ext_arg):
         return ap_ar_factory.apply(con, ext_type, ext_arg)
     elif process == "o2c":
         return o2c_factory.apply(con, ext_type, ext_arg)
+    elif process == "p2p":
+        return p2p_factory.apply(con, ext_type, ext_arg)
