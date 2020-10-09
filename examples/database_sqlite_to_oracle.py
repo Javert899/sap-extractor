7f74fc7e966832e41a6dd895fff994cd1adb801c
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-10-09 08:27:14 +0200

    made script working
    made script working

--- examples/database_sqlite_to_oracle.py
+++ examples/database_sqlite_to_oracle.py
@@ -1,12 +1,13 @@
 import sapextractor
 import traceback
+from sapextractor.utils import constants
 
 
 def execute_script():
     c1 = sapextractor.connect_sqlite("../sap.sqlite")
     c2 = sapextractor.connect_oracle(hostname="127.0.0.1", port="1521", sid="xe", username="system", password="oracle")
-    list_tables = c1.get_list_tables()
-    for table in list_tables:
+    #list_tables = c1.get_list_tables()
+    for table in constants.INVOLVED_TABLES:
         try:
             print()
             print(table)
--- sapextractor/utils/constants.py
+++ sapextractor/utils/constants.py
@@ -9,3 +9,20 @@ def set_documents(content):
         if str(x).lower() != "nan":
             ret.append(x)
     return ret
+
+
+INVOLVED_TABLES = [
+    "T003T",
+    "CDHDR",
+    "CDPOS",
+    "TSTCT",
+    "DD07T",
+    "BKPF",
+    "BSEG",
+    "VBFA",
+    "EBAN",
+    "EKKO",
+    "EKPO",
+    "MKPF",
+    "MSEG"
+]
