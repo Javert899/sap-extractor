cb867df112f678e3871ce5b624afbe59c220c022
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-02-14 14:31:33 +0100

    small changes to the mapping
    small changes to the mapping

--- sapextractor/utils/change_tables/mapping.py
+++ sapextractor/utils/change_tables/mapping.py
@@ -80,18 +80,18 @@ def perform_mapping(tabname, fname, value_old, value_new, chngind, fnames, retur
         except:
             value_old = 0
         if value_old == 0:
-            return "Set Billing Date", 1
+            return "Set Billing Date", 2
         elif value_new == 0:
-            return "Remove Billing Date", 1
+            return "Remove Billing Date", 2
         elif value_new <= value_old:
             return "Anticipate Billing Date", 1
         else:
             return "Postpone Billing Date", 1
     elif fname == "FAKSK":
         if value_new is None:
-            return "Remove Billing Block", 1
+            return "Remove Billing Block", 2
         else:
-            return "Set Billing Block", 1
+            return "Set Billing Block", 2
     elif fname == "SKFBP":
         value_new = float(value_new)
         value_old = float(value_old)
@@ -119,9 +119,9 @@ def perform_mapping(tabname, fname, value_old, value_new, chngind, fnames, retur
         except:
             value_old = 0
         if value_old == 0:
-            return "Set Actual Goods Movement Date", 1
+            return "Set Actual Goods Movement Date", 2
         elif value_new == 0:
-            return "Remove Actual Goods Movement Date", 1
+            return "Remove Actual Goods Movement Date", 2
         elif value_new <= value_old:
             return "Anticipate Actual Goods Movement Date", 1
         else:
