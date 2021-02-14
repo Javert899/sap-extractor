a481b51ad630e16004236b709ee426f639424103
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-02-14 14:30:33 +0100

    changed act count in graph

--- sapextractor/algo/o2c/graph_retrieval_util.py
+++ sapextractor/algo/o2c/graph_retrieval_util.py
@@ -10,9 +10,23 @@ def extract_dfg(con):
     vbfa = vbfa.dropna(subset=["VBTYP_V", "VBTYP_N"], how="any")
     vbfa = vbfa.to_dict("r")
     dfg = {(x["VBTYP_V"], x["VBTYP_N"]): int(x["COUNT"]) for x in vbfa}
-    act_count = {}
+    act_count_exit = {}
     for el in dfg:
-        if not el[0] in act_count:
-            act_count[el[0]] = 0
-        act_count[el[0]] += int(dfg[el])
+        if not el[0] in act_count_exit:
+            act_count_exit[el[0]] = 0
+        act_count_exit[el[0]] += int(dfg[el])
+    act_count_entry = {}
+    for el in dfg:
+        if not el[1] in act_count_entry:
+            act_count_entry[el[1]] = 0
+        act_count_entry[el[1]] += int(dfg[el])
+    act_count = {}
+    activities = set(act_count_entry).union(set(act_count_exit))
+    for act in activities:
+        if act in act_count_entry and act in act_count_exit:
+            act_count[act] = max(act_count_entry[act], act_count_exit[act])
+        elif act in act_count_entry:
+            act_count[act] = act_count_entry[act]
+        elif act in act_count_exit:
+            act_count[act] = act_count_exit[act]
     return dfg, act_count
