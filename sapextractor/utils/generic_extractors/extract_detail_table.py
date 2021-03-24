a79c6a45578c3562ab56fa3de730ecb549ddf909
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-03-24 10:34:52 +0100

    intermediate commit

--- sapextractor/utils/generic_extractors/extract_detail_table.py
+++ sapextractor/utils/generic_extractors/extract_detail_table.py
@@ -4,7 +4,7 @@ import pandas as pd
 from pm4pymdl.algo.mvp.utils import exploded_mdl_to_succint_mdl
 
 
-def extract_detail_table(con, tab_name, mandt="800", key_spec=None, min_unq_values=10):
+def extract_detail_table(cache, con, tab_name, mandt="800", key_spec=None, min_unq_values=100):
     if key_spec is None:
         key_spec = {}
     primary_keys, foreign_keys, timestamp_resource, fields_with_type, fname_checktable = meaningful_fields.apply_static(con, tab_name)
@@ -22,4 +22,17 @@ def extract_detail_table(con, tab_name, mandt="800", key_spec=None, min_unq_valu
         if unq_values >= min_unq_values:
             allowed_cols.append(c)
     df = df[allowed_cols]
-    return df
+    df.columns = ["event_"+x for x in df.columns]
+    prim_keys_in_df = tuple(x for x in df.columns if x in primary_keys)
+    not_prim_keys_in_df = tuple(x for x in df.columns if x not in primary_keys)
+    list_dfs = []
+    for k in not_prim_keys_in_df:
+        df2 = df[list(prim_keys_in_df)+[k]].dropna(subset=[k])
+        df2 = df2[df2[k] != " "]
+        list_dfs.append(df2)
+    df = pd.concat(list_dfs)
+    df = df.set_index(list(prim_keys_in_df))
+    if prim_keys_in_df not in cache:
+        cache[prim_keys_in_df] = {}
+    cache[prim_keys_in_df][tab_name] = (not_prim_keys_in_df, fields_with_type, df)
+
--- sapextractor/utils/generic_extractors/extract_event_table.py
+++ sapextractor/utils/generic_extractors/extract_event_table.py
@@ -8,7 +8,6 @@ def basic_extraction(con, tab_name, mandt="800", key_spec=None, min_unq_values=1
     if key_spec is None:
         key_spec = {}
     primary_keys, foreign_keys, timestamp_resource, fields_with_type, fname_checktable = meaningful_fields.apply_static(con, tab_name)
-    print(fname_checktable)
     fields = [x for x in fields_with_type if x != "event_CUSTOMOBJECTID"]
     fields = [x for x in fields if x in primary_keys or x in foreign_keys or x in timestamp_resource.values()]
     fields = [x.split("event_")[1] for x in fields]
@@ -36,7 +35,7 @@ def basic_extraction(con, tab_name, mandt="800", key_spec=None, min_unq_values=1
     return df
 
 
-def apply(con, tab_name, mandt="800", key_spec=None, min_unq_values=10):
+def apply(cache, con, tab_name, mandt="800", key_spec=None, min_unq_values=100):
     primary_keys, foreign_keys, timestamp_resource, fields_with_type, fname_checktable = meaningful_fields.apply_static(con, tab_name)
     fields_with_type["event_CUSTOMOBJECTID"] = "AWKEY"
 
@@ -54,6 +53,28 @@ def apply(con, tab_name, mandt="800", key_spec=None, min_unq_values=10):
     keys_to_consider = keys_to_consider.union({"event_CUSTOMOBJECTID"})
     types_to_consider = set(fields_with_type[x] for x in keys_to_consider)
     list_dfs = []
+    primary_keys = tuple(primary_keys)
+    if primary_keys in cache:
+        df = df.set_index(list(primary_keys))
+        for tab_name in cache[primary_keys]:
+            not_prim_keys_in_df, join_fields_with_type, df2 = cache[primary_keys][tab_name]
+            df2 = df.join(df2, rsuffix="_2")
+            df2 = df2.reset_index(drop=True)
+            join_types_to_consider = {}
+            for x in not_prim_keys_in_df:
+                this_type = join_fields_with_type[x]
+                if this_type not in join_types_to_consider:
+                    join_types_to_consider[this_type] = set()
+                join_types_to_consider[this_type].add(x)
+            for t in join_types_to_consider:
+                for f in join_types_to_consider[t]:
+                    if f in df2.columns:
+                        df3 = df2.dropna(subset=[f])
+                        df3[f] = df3[f].astype(str)
+                        df3 = df3[df3[f] != " "]
+                        df3[t] = df3[f]
+                        if len(df3) > 0:
+                            list_dfs.append(df3)
     for t in types_to_consider:
         int_df = []
         for f in inv_fields_with_type[t]:
@@ -66,9 +87,9 @@ def apply(con, tab_name, mandt="800", key_spec=None, min_unq_values=10):
                     int_df.append(df2)
         if int_df:
             df3 = pd.concat(int_df)
-            df3 = df3.groupby(t).first().reset_index()
+            df3 = df3.groupby(t).first().reset_index(drop=True)
             list_dfs.append(df3)
-    df = pd.concat(list_dfs)
-    df.type = "exploded"
-    df = exploded_mdl_to_succint_mdl.apply(df)
-    return df
+    df4 = pd.concat(list_dfs)
+    df4.type = "exploded"
+    df4 = exploded_mdl_to_succint_mdl.apply(df4)
+    return df4
