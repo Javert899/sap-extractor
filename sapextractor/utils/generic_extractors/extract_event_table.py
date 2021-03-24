ea577f9a94b1c0750a9ea6d4b0bc05319f42584a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-03-24 09:29:53 +0100

    extract_event_table

--- sapextractor/utils/generic_extractors/extract_event_table.py
+++ sapextractor/utils/generic_extractors/extract_event_table.py
@@ -1,12 +1,16 @@
 from sapextractor.utils.table_meaningful_fields import meaningful_fields
 import uuid
+import pandas as pd
+from pm4pymdl.algo.mvp.utils import exploded_mdl_to_succint_mdl
 
 
 def basic_extraction(con, tab_name, mandt="800", key_spec=None):
     if key_spec is None:
         key_spec = {}
     primary_keys, foreign_keys, timestamp_resource, fields_with_type = meaningful_fields.apply_static(con, tab_name)
-    fields = [x.split("event_")[1] for x in fields_with_type]
+    fields = [x for x in fields_with_type if x != "CUSTOMOBJECTID"]
+    fields = [x for x in fields if x in primary_keys or x in foreign_keys or x in timestamp_resource.values()]
+    fields = [x.split("event_")[1] for x in fields]
     query = "SELECT "+",".join(fields)+" FROM "+con.table_prefix+tab_name+" WHERE MANDT = '"+mandt+"'"
     for key in primary_keys:
         if key in key_spec:
@@ -14,11 +18,49 @@ def basic_extraction(con, tab_name, mandt="800", key_spec=None):
     df = con.execute_read_sql(query, fields)
     df.columns = ["event_" + x for x in df.columns]
     df["event_id"] = df.apply(lambda _: str(uuid.uuid4()), axis=1)
+    df["event_activity"] = "dummy"
+    df["event_timestamp"] = df[timestamp_resource["DATUM"]]
     df["event_CUSTOMOBJECTID"] = ""
     for idx, key in enumerate(primary_keys):
         if idx > 0:
             df["event_CUSTOMOBJECTID"] = "," + df[key]
         else:
             df["event_CUSTOMOBJECTID"] = df[key]
+    return df
+
+
+def apply(con, tab_name, mandt="800", key_spec=None):
+    primary_keys, foreign_keys, timestamp_resource, fields_with_type = meaningful_fields.apply_static(con, tab_name)
     fields_with_type["event_CUSTOMOBJECTID"] = "AWKEY"
+
+    inv_fields_with_type = {}
+    for x, y in fields_with_type.items():
+        if y not in inv_fields_with_type:
+            inv_fields_with_type[y] = set()
+        inv_fields_with_type[y].add(x)
+
+    df = basic_extraction(con, tab_name, mandt=mandt, key_spec=key_spec)
+    df["event_timestamp"] = pd.to_datetime(df["event_timestamp"], format = con.DATE_FORMAT, errors="coerce")
+    df = df.dropna(subset=["event_timestamp"])
+    keys_to_consider = set(primary_keys)
+    keys_to_consider = keys_to_consider.union(foreign_keys)
+    keys_to_consider = keys_to_consider.union({"event_CUSTOMOBJECTID"})
+    types_to_consider = set(fields_with_type[x] for x in keys_to_consider)
+    list_dfs = []
+    for t in types_to_consider:
+        int_df = []
+        for f in inv_fields_with_type[t]:
+            df2 = df.dropna(subset=[f])
+            df2[f] = df2[f].astype(str)
+            df2 = df2[df2[f] != " "]
+            df2[t] = df2[f]
+            if len(df2) > 0:
+                int_df.append(df2)
+        if int_df:
+            df3 = pd.concat(int_df)
+            df3 = df3.groupby(t).first().reset_index()
+            list_dfs.append(df3)
+    df = pd.concat(list_dfs)
+    df.type = "exploded"
+    df = exploded_mdl_to_succint_mdl.apply(df)
     return df
--- sapextractor/utils/main_domnames/extract_main_domnames.py
+++ sapextractor/utils/main_domnames/extract_main_domnames.py
@@ -1,11 +1,13 @@
 from copy import copy
+import pandas as pd
 
 class Shared:
     domnames_dictio = {}
 
 
 def apply(con, min_count=1000):
-    df = con.execute_read_sql("SELECT DOMNAME, Count(*) FROM "+con.table_prefix+"DD03VV GROUP BY DOMNAME ORDER BY Count(*) DESC", ["DOMNAME", "COUNT"])
+    #df = con.execute_read_sql("SELECT DOMNAME, Count(*) FROM "+con.table_prefix+"DD03VV GROUP BY DOMNAME ORDER BY Count(*) DESC", ["DOMNAME", "COUNT"])
+    df = pd.read_csv("DOMNAME.csv")
     df = df[df["DOMNAME"] != " "]
     df = df[df["COUNT"] >= 1000]
     return set(df["DOMNAME"].unique())
