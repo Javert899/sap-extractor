9a4a4e7e14b90eb2a9f9bbd1c95f4beabb41f768
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-03-24 12:36:34 +0100

    expand set

--- sapextractor/utils/table_expansion/expand.py
+++ sapextractor/utils/table_expansion/expand.py
@@ -2,3 +2,10 @@ def expand(con, tab):
     df = con.execute_read_sql("SELECT CHECKTABLE FROM "+con.table_prefix+"DD03VV WHERE TABNAME = '"+tab+"' AND TABCLASS = 'TRANSP'", ["CHECKTABLE"])
     df = df[df["CHECKTABLE"] != " "]
     return set(df["CHECKTABLE"].unique())
+
+
+def expand_set(con, tab_set):
+    new_set = set(tab_set)
+    for tab in tab_set:
+        new_set = new_set.union(expand(con, tab))
+    return new_set
