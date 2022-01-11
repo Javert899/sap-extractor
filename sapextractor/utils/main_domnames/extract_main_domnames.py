69478226c0fd5ed3c04925445f10e6527cc5909f
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-01-11 13:14:04 +0100

    extracting the main domain names

--- sapextractor/utils/main_domnames/extract_main_domnames.py
+++ sapextractor/utils/main_domnames/extract_main_domnames.py
@@ -6,11 +6,10 @@ class Shared:
 
 
 def apply(con, min_count=1000):
-    df = con.execute_read_sql("SELECT DOMNAME, Count(*) FROM "+con.table_prefix+"DD03VV GROUP BY DOMNAME ORDER BY Count(*) DESC", ["DOMNAME", "COUNT"])
-    #df = pd.read_csv("DOMNAME.csv")
+    df = con.execute_read_sql("SELECT DOMNAME, Count(*) FROM "+con.table_prefix+"DD03VV where tabname IN(Select Tabname from SAPSR3.DD02V A join SAPSR3.DBSTATTORA B on A.tabname = B.tname where tabclass='TRANSP' and ddlanguage = 'E' and contflag = 'A' and NROWS > 0) GROUP BY DOMNAME ORDER BY Count(*) DESC", ["DOMNAME", "COUNT"])
     df = df[df["DOMNAME"] != " "]
-    df = df[df["COUNT"] >= 1000]
     return set(df["DOMNAME"].unique())
+
 
 
 def apply_static(con, min_count=1000):
