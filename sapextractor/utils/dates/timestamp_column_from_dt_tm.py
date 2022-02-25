589c47c51500e1b3acd1996960997940792f6898
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-02-25 13:39:53 +0100

    trial

--- sapextractor/utils/dates/timestamp_column_from_dt_tm.py
+++ sapextractor/utils/dates/timestamp_column_from_dt_tm.py
@@ -6,14 +6,16 @@ import traceback
 
 def apply(dataframe, dt_column, tm_column, target_column):
     try:
-        dataframe[dt_column] = pd.to_datetime(dataframe[dt_column], format=constants.DATE_FORMAT_INTERNAL)
+        dataframe[dt_column] = pd.to_datetime(dataframe[dt_column], format=constants.DATE_FORMAT_INTERNAL, errors='coerce')
+        dataframe = dataframe.dropna(subset=[dt_column])
     except:
         print("a failed")
         print(dataframe[dt_column])
         traceback.print_exc()
 
     try:
-        dataframe[tm_column] = pd.to_datetime(dataframe[tm_column], format=constants.HOUR_FORMAT_INTERNAL)
+        dataframe[tm_column] = pd.to_datetime(dataframe[tm_column], format=constants.HOUR_FORMAT_INTERNAL, errors='coerce')
+        dataframe = dataframe.dropna(subset=[tm_column])
     except:
         print("b failed")
         print(dataframe[tm_column])
