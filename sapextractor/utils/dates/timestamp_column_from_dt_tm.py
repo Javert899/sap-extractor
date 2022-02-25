cf84d404ef648252559f8323635c93d78b6e6174
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-02-25 13:57:38 +0100

    trial

--- sapextractor/utils/dates/timestamp_column_from_dt_tm.py
+++ sapextractor/utils/dates/timestamp_column_from_dt_tm.py
@@ -5,30 +5,21 @@ import traceback
 
 
 def apply(dataframe, dt_column, tm_column, target_column):
-    try:
+    if "str" in str(dataframe[dt_column].dtype) or "obj" in str(dataframe[dt_column].dtype):
         dataframe[dt_column] = pd.to_datetime(dataframe[dt_column], format=constants.DATE_FORMAT_INTERNAL, errors='coerce')
         dataframe = dataframe.dropna(subset=[dt_column])
-    except:
-        print("a failed")
-        print(dataframe[dt_column])
-        traceback.print_exc()
 
-    try:
+    if "str" in str(dataframe[tm_column].dtype) or "obj" in str(dataframe[tm_column].dtype):
         dataframe[tm_column] = pd.to_datetime(dataframe[tm_column], format=constants.HOUR_FORMAT_INTERNAL, errors='coerce')
         dataframe = dataframe.dropna(subset=[tm_column])
-    except:
-        print("b failed")
-        print(dataframe[tm_column])
-        traceback.print_exc()
-
-
-    dataframe[dt_column] = dataframe[dt_column].apply(
-        lambda x: datetime(year=x.year, month=x.month, day=x.day).timestamp())
-    dataframe[tm_column] = dataframe[tm_column].apply(lambda x: x.timestamp())
-
-    dataframe[tm_column] = dataframe[tm_column] + 2208988800
 
     if len(dataframe) > 0:
+        dataframe[dt_column] = dataframe[dt_column].apply(
+            lambda x: datetime(year=x.year, month=x.month, day=x.day).timestamp())
+        dataframe[tm_column] = dataframe[tm_column].apply(lambda x: x.timestamp())
+
+        dataframe[tm_column] = dataframe[tm_column] + 2208988800
+
         dataframe[target_column] = dataframe[dt_column] + dataframe[tm_column]
         dataframe[target_column] = dataframe[target_column].apply(lambda x: datetime.fromtimestamp(x))
         dataframe = dataframe.sort_values("event_timestamp")
