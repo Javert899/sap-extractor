30b9732ab929cc30896e2bc514fd07459f49c864
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-04-21 13:53:47 +0200

    RSEG join

--- sapextractor/incremental_p2p/01_form_query.py
+++ sapextractor/incremental_p2p/01_form_query.py
@@ -45,11 +45,30 @@ def form_eban_query(eban_name="eban"):
     return " ".join(ret), columns
 
 
-def form_total_query(ekpo_name="a", eban_name="b"):
+def form_rseg_query(rseg_name="rseg", rbkp_name="rbkp"):
+    ret = ["SELECT "+rseg_name+".RSEG_ROW_NUMBER AS RSEG_ROW_NUMBER, "+rseg_name+".MANDT AS MANDT, "+rseg_name+".BUKRS AS BUKRS, "+rseg_name+".GJAHR AS GJAHR, "+rseg_name+".BELNR AS BELNR, "+rseg_name+".BUZEI AS BUZEI, "+rseg_name+".EBELN AS EBELN, "+rseg_name+".EBELP AS EBELP, "+rbkp_name+".BLDAT AS BLDAT, "+rbkp_name+".BUDAT AS BUDAT, "+rbkp_name+".USNAM AS USNAM, "+rbkp_name+".TCODE AS TCODE, "+rbkp_name+".LIFNR AS LIFNR"]
+    ret.append("FROM (")
+    ret.append("SELECT "+row_number()+" AS RSEG_ROW_NUMBER, MANDT, BUKRS, GJAHR, BELNR, BUZEI, EBELN, EBELP FROM")
+    ret.append(parameters["prefix"]+"RSEG")
+    ret.append("WHERE "+row_number()+" >= "+get_table_count("RSEG"))
+    ret.append(") "+rseg_name+" JOIN (")
+    ret.append("SELECT MANDT, BUKRS, GJAHR, BELNR, BLDAT, BUDAT, USNAM, TCODE, LIFNR FROM")
+    ret.append(parameters["prefix"]+"RBKP")
+    ret.append(") "+rbkp_name+" ON")
+    ret.append(rseg_name+".MANDT = "+rbkp_name+".MANDT AND")
+    ret.append(rseg_name+".BUKRS = "+rbkp_name+".BUKRS AND")
+    ret.append(rseg_name+".GJAHR = "+rbkp_name+".GJAHR AND")
+    ret.append(rseg_name+".BELNR = "+rbkp_name+".BELNR")
+    columns = ["RSEG_ROW_NUMBER", "MANDT", "BUKRS", "GJAHR", "BELNR", "BUZEI", "EBELN", "EBELP", "BLDAT", "BUDAT", "USNAM", "TCODE", "LIFNR"]
+    return " ".join(ret), columns
+
+
+def form_total_query(ekpo_name="a", eban_name="b", rseg_name="c"):
     fields = []
 
     ekpo_query, ekpo_columns = form_ekpo_query()
     eban_query, eban_columns = form_eban_query()
+    rseg_query, rseg_columns = form_rseg_query()
 
     fields.append(ekpo_name+".EKPO_ROW_NUMBER")
     fields.append(ekpo_name+".MANDT")
@@ -65,6 +84,16 @@ def form_total_query(ekpo_name="a", eban_name="b"):
     fields.append(eban_name+".BANFNBNFPO")
     fields.append(eban_name+".ERNAM")
     fields.append(eban_name+".BADAT")
+    fields.append(rseg_name+".RSEG_ROW_NUMBER")
+    fields.append(rseg_name+".BUKRS")
+    fields.append(rseg_name+".GJAHR")
+    fields.append(rseg_name+".BELNR")
+    fields.append(rseg_name+".BUZEI")
+    fields.append(rseg_name+".BLDAT")
+    fields.append(rseg_name+".BUDAT")
+    fields.append(rseg_name+".USNAM")
+    fields.append(rseg_name+".TCODE")
+    fields.append(rseg_name+".LIFNR")
 
     ret = ["SELECT "]
     ret.append(", ".join(fields))
@@ -77,6 +106,12 @@ def form_total_query(ekpo_name="a", eban_name="b"):
     ret.append("ON "+ekpo_name+".MANDT = "+eban_name+".MANDT")
     ret.append("AND "+ekpo_name+".BANFN = "+eban_name+".BANFN")
     ret.append("AND "+ekpo_name+".BNFPO = "+eban_name+".BNFPO")
+    ret.append("FULL OUTER JOIN (")
+    ret.append(rseg_query)
+    ret.append(") "+rseg_name)
+    ret.append("ON "+ekpo_name+".MANDT = "+rseg_name+".MANDT")
+    ret.append("AND "+ekpo_name+".EBELN = "+rseg_name+".EBELN")
+    ret.append("AND "+ekpo_name+".EBELP = "+rseg_name+".EBELP")
 
     return " ".join(ret), fields
 
@@ -84,6 +119,7 @@ def form_total_query(ekpo_name="a", eban_name="b"):
 if __name__ == "__main__":
     ekpo_query, ekpo_columns = form_ekpo_query()
     eban_query, eban_columns = form_eban_query()
+    rseg_query, rseg_columns = form_rseg_query()
 
     total_query, total_columns = form_total_query()
 
@@ -93,3 +129,4 @@ if __name__ == "__main__":
     dataframe = c.execute_read_sql(total_query, total_columns)
     print(dataframe)
     print(dataframe.columns)
+    dataframe.to_csv("prova.csv", index=False)
